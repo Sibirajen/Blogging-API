752c1dd8426ecbb28da28a058a67441631ece968
Sibirajen <Sibirajen16@gmail.com>
2025-06-19 19:56:34 +0530

    Initial commit

---
+++ src/main/java/com/sibirajen/Blogging/API/BloggingApiApplication.java
@@ -0,0 +1,13 @@
+package com.sibirajen.Blogging.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApiApplication.class, args);
+	}
+
+}
