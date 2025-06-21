d7cfce0eb4da5e82af7cc6b58948baa5d63e34f0
Sibirajen <Sibirajen16@gmail.com>
2025-06-21 11:32:17 +0530

    Added BlogRepository

---
+++ src/main/java/com/sibirajen/Blogging/API/repo/BlogRepository.java
@@ -0,0 +1,10 @@
+package com.sibirajen.Blogging.API.repo;
+
+import com.sibirajen.Blogging.API.model.Blog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends CrudRepository<Blog, Long> {
+
+}
