e705a6b61c4aca74022da367a08c7250cdb7979b
Sibirajen <Sibirajen16@gmail.com>
2025-06-21 13:52:33 +0530

    Changed this interface to extend JPARepository<T, ID> instead of CurdRepository<T, ID>

--- src/main/java/com/sibirajen/Blogging/API/blog/BlogRepository.java
+++ src/main/java/com/sibirajen/Blogging/API/blog/BlogRepository.java
@@ -1,10 +1,10 @@
 package com.sibirajen.Blogging.API.blog;
 
 import com.sibirajen.Blogging.API.model.Blog;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlogRepository extends CrudRepository<Blog, Long> {
+public interface BlogRepository extends JpaRepository<Blog, Long> {
 
 }
