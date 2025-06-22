f684860408a4a2878cb7cd61d14cacc3c8c67a5c
Sibirajen <Sibirajen16@gmail.com>
2025-06-22 23:15:48 +0530

    Added /posts post endpoint

--- src/main/java/com/sibirajen/Blogging/API/blog/BlogComponent.java
+++ src/main/java/com/sibirajen/Blogging/API/blog/BlogComponent.java
@@ -1,22 +1,30 @@
 package com.sibirajen.Blogging.API.blog;
 
+import com.sibirajen.Blogging.API.dto.BlogRequest;
+import com.sibirajen.Blogging.API.dto.BlogResponse;
+import com.sibirajen.Blogging.API.model.Blog;
+import com.sibirajen.Blogging.API.tag.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class BlogComponent {
-    private final BlogRepository repo;
+    private final BlogRepository blogRepo;
+    private final TagRepository tagRepo;
 
     @Autowired
-    public BlogComponent(BlogRepository repo) {
-        this.repo = repo;
+    public BlogComponent(BlogRepository blogRepo, TagRepository tagRepo) {
+        this.blogRepo = blogRepo;
+        this.tagRepo = tagRepo;
     }
 
     @InitBinder
@@ -26,26 +34,33 @@ public class BlogComponent {
 
     @GetMapping("/posts/{id}")
     public ResponseEntity<BlogResponse> getBlogById(@PathVariable Long id){
-        return repo.findById(id)
-                .map(BlogResponseMapper::toDTO)
+        return blogRepo.findById(id)
+                .map(BlogMapper::toBlogResponse)
                 .map(ResponseEntity::ok)
                 .orElseGet( () -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping("/posts")
     public ResponseEntity<List<BlogResponse>> getBlogs(){
-        List<BlogResponse> responseList = repo.findAll().stream()
-                .map(BlogResponseMapper::toDTO)
+        List<BlogResponse> responseList = blogRepo.findAll().stream()
+                .map(BlogMapper::toBlogResponse)
                 .toList();
         return ResponseEntity.ok(responseList);
     }
 
     @DeleteMapping("/posts/{id}")
     public ResponseEntity<Void> deleteBlogById(@PathVariable Long id){
-        if(!repo.existsById(id)) {
+        if(!blogRepo.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        repo.deleteById(id);
+        blogRepo.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<BlogResponse> postBlog(@Validated @RequestBody BlogRequest blogRequest){
+        Blog savedBlog = blogRepo.save(BlogMapper.toBlog(blogRequest, tagRepo));
+        URI uri = URI.create("/posts/" + savedBlog.getId());
+        return ResponseEntity.created(uri).body(BlogMapper.toBlogResponse(savedBlog));
     }
 }
