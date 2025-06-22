973768c97f23b9d17c66d9f95f385ec711a57f00
Sibirajen <Sibirajen16@gmail.com>
2025-06-22 23:15:08 +0530

    Added TagRepository

---
+++ src/main/java/com/sibirajen/Blogging/API/tag/TagRepository.java
@@ -0,0 +1,12 @@
+package com.sibirajen.Blogging.API.tag;
+
+import com.sibirajen.Blogging.API.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByTag(String tag);
+}
