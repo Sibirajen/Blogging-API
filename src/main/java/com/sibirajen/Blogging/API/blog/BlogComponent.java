e6c687d38f488ce701b9eed35f73f5bf203d71fb
Sibirajen <Sibirajen16@gmail.com>
2025-06-23 22:54:17 +0530

    Added put endpoint "/posts/{id}"

--- src/main/java/com/sibirajen/Blogging/API/blog/BlogComponent.java
+++ src/main/java/com/sibirajen/Blogging/API/blog/BlogComponent.java
@@ -3,7 +3,9 @@ package com.sibirajen.Blogging.API.blog;
 import com.sibirajen.Blogging.API.dto.BlogRequest;
 import com.sibirajen.Blogging.API.dto.BlogResponse;
 import com.sibirajen.Blogging.API.model.Blog;
+import com.sibirajen.Blogging.API.model.Tag;
 import com.sibirajen.Blogging.API.tag.TagRepository;
+import com.sibirajen.Blogging.API.tag.TagResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -62,5 +65,25 @@ public class BlogComponent {
         Blog savedBlog = blogRepo.save(BlogMapper.toBlog(blogRequest, tagRepo));
         URI uri = URI.create("/posts/" + savedBlog.getId());
         return ResponseEntity.created(uri).body(BlogMapper.toBlogResponse(savedBlog));
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<BlogResponse> putBlog(@PathVariable Long id, @Validated @RequestBody BlogRequest blogRequest){
+        Optional<Blog> savedBlogOpt = blogRepo.findById(id);
+        if(savedBlogOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        Blog savedBlog = savedBlogOpt.get();
+
+        List<Tag> tagList = TagResolver.toTagList(blogRequest.getTags(), tagRepo);
+
+        savedBlog.setTitle(blogRequest.getTitle());
+        savedBlog.setContent(blogRequest.getContent());
+        savedBlog.setCategory(blogRequest.getCategory());
+        savedBlog.setTags(tagList);
+
+        BlogResponse blogResponse = BlogMapper.toBlogResponse(blogRepo.save(savedBlog));
+        return ResponseEntity.ok(blogResponse);
     }
 }
