986b5ab2b70b0a794a6cbabb530d59791b2cd772
Sibirajen <Sibirajen16@gmail.com>
2025-06-24 11:20:38 +0530

    Added custom query method to search through data

--- src/main/java/com/sibirajen/Blogging/API/blog/BlogRepository.java
+++ src/main/java/com/sibirajen/Blogging/API/blog/BlogRepository.java
@@ -4,7 +4,10 @@ import com.sibirajen.Blogging.API.model.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
+    List<Blog> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrCategoryContainingIgnoreCase(String title, String content, String category);
 
 }
